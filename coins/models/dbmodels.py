20cf6481d516f4d0989ccc8142fb80a137bcde83
Koorosh Rezaee Araghpour <bboykoorosh@gmail.com>
2022-07-15 03:25:41 +0430

    add: adding unique constraint on coins and coin_id_contract_address

--- coins/models/dbmodels.py
+++ coins/models/dbmodels.py
@@ -1,5 +1,4 @@
-from operator import index
-from sqlalchemy import Column, ForeignKey, String, Enum, Integer, DateTime
+from sqlalchemy import Column, ForeignKey, String, Enum, Integer, DateTime, func, UniqueConstraint
 from sqlalchemy import func
 from sqlalchemy.orm import relationship
 
@@ -10,11 +9,12 @@ from coins.models.database import Base
 class Coins(Base):
     __tablename__ = "coins"
     id = Column(Integer, primary_key=True)
-    coin_id = Column(String, index=True)
+    coin_id = Column(String, index=True, unique=True)
     coin_symbol = Column(String, index=True)
     coin_name = Column(String)
     contract_addresses = relationship("CoinsContract", back_populates="coin")
     created = Column(DateTime, server_default=func.now())
+    
     
 class CoinsContract(Base):
     __tablename__ = "coins_contract"
@@ -25,3 +25,5 @@ class CoinsContract(Base):
     coin_id = Column(Integer, ForeignKey("coins.id"))
     coin = relationship("Coins", back_populates="contract_addresses")
     created = Column(DateTime, server_default=func.now())
+    
+    UniqueConstraint(coin_id, contract_address, name="uc_coin_id_contract_address")
