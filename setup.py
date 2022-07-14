1f0cebe9d6de27e9bf2cbd3267c2f6c9f896330a
Koorosh Rezaee Araghpour <bboykoorosh@gmail.com>
2022-07-14 21:58:29 +0430

    init1

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='coins',
+    version='0.1.0',
+    packages=['coins'],
+    url='https://github.com/koorosh-rezaee/coins',
+    license='GNU GENERAL PUBLIC LICENSE',
+    author='Koorosh Rezaee',
+    author_email='bboykoorosh@gmail.com',
+    description='A mini project to Fetch, Store, Update, and Retrieve different Tokens data using Celery, FastAPI,  Postgresql, SQLAlchemy, and Redis'
+)
