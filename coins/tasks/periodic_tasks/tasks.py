b83012273dfa98fcb8e6e339e636c6e497fc9f5a
Koorosh Rezaee Araghpour <bboykoorosh@gmail.com>
2022-07-15 17:50:56 +0430

    fix: adding some comments

--- coins/tasks/periodic_tasks/tasks.py
+++ coins/tasks/periodic_tasks/tasks.py
@@ -21,7 +21,7 @@ from coins.core.config import settings
 @app.on_after_finalize.connect
 def setup_periodic_tasks(sender, **kwargs):
     
-    # Calls produce_bnb_watcher_tasks every watcher_period_seconds seconds
+    # Calls fetch_and_update_under_watch_tokens_prices to produce price updating tasks every (price_watcher_interval_seconds) seconds
     sender.add_periodic_task(settings.price_watcher_interval_seconds, fetch_and_update_under_watch_tokens_prices.s(), name='Update Tokens Prices')      
 
 
