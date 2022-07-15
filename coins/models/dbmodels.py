1fb9255e09f9e5d68a0d085cb32001a8d93d35e8
Koorosh Rezaee Araghpour <bboykoorosh@gmail.com>
2022-07-15 17:07:19 +0430

    add: adding watch for price field to offload the apicalls to fetch prices

--- coins/models/dbmodels.py
+++ coins/models/dbmodels.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, ForeignKey, String, Enum, Integer, DateTime, func, UniqueConstraint
+from sqlalchemy import Column, ForeignKey, String, Enum, Integer, DateTime, func, UniqueConstraint, Boolean
 from sqlalchemy import func
 from sqlalchemy.orm import relationship
 
@@ -12,6 +12,7 @@ class Coins(Base):
     coin_id = Column(String, index=True, unique=True)
     coin_symbol = Column(String, index=True)
     coin_name = Column(String)
+    watch_for_price = Column(Boolean, default=False)
     contract_addresses = relationship("CoinsContract", back_populates="coin")
     created = Column(DateTime, server_default=func.now())
     
