742ffd07fb8038b41ad43612184ab1a1e02cdfe9
Koorosh Rezaee Araghpour <bboykoorosh@gmail.com>
2022-07-17 02:34:29 +0430

    add: adding the dynamic ip resolver to support WSL

--- start-coins.sh
+++ start-coins.sh
@@ -1,10 +1,23 @@
+export MYIP=`ifconfig | grep -A 1 eth0 | grep inet | cut -f 2 -d "t" | cut -f 2 -d " "`
+
 sudo docker rmi devico-coins
 
 sudo docker build -t devico-coins .
 
 sudo docker compose up -d
 
-
-echo "checkout the api docs at: http://127.0.0.1:80/docs"
-echo "checkout the graphql at: http://127.0.0.1:80/graphql"
-echo "checkout the rabbitmq status at: http://127.0.0.1:15672 with the user: guest and the password: guest"
+if [ $MYIP ]; then
+    echo "checkout the api docs at: http://$MYIP/docs"
+else
+    echo "checkout the api docs at: http://127.0.0.1:80/docs"
+fi
+if [ $MYIP ]; then
+    echo "checkout the graphql at: http://$MYIP:80/graphql"
+else
+    echo "checkout the graphql at: http://127.0.0.1:80/graphql"
+fi
+if [ $MYIP ]; then
+    echo "checkout the rabbitmq status at: http://$MYIP:15672 with the user: guest and the password: guest"
+else
+    echo "checkout the rabbitmq status at: http://127.0.0.1:15672 with the user: guest and the password: guest"
+fi
