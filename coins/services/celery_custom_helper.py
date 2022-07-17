1c2e145697b6a9c6124be7396ebb9f73d3e89fc0
Koorosh Rezaee Araghpour <bboykoorosh@gmail.com>
2022-07-17 16:16:06 +0430

    add: adding a helper to get the progress of celery tasks

--- coins/services/celery_custom_helper.py
+++ coins/services/celery_custom_helper.py
@@ -1,4 +1,4 @@
-from celery_app import app
+from coins.celery_app import app
 from loguru import logger
 
 
@@ -14,4 +14,32 @@ def revoke_task_ids(task_ids: list, force: bool=False):
         return True
     except Exception as e:
         logger.error(f" [x] Could not revoke {len(task_ids)} task(s) because: {e}")
-        
+
+
+def get_task_ids_progress(task_ids: list) -> dict:
+    
+    total = len(task_ids)
+    progress = {
+        'total': total,
+        'ready': 0,
+        'successful': 0,
+        'failed': 0
+    }
+    
+    try:
+        for task_id in task_ids:
+            
+            if app.AsyncResult(task_id).ready():
+                progress['ready'] += 1
+                
+                if app.AsyncResult(task_id).successful():
+                    progress['successful'] += 1
+                elif app.AsyncResult(task_id).failed():
+                    progress['failed'] += 1
+                    
+        return progress
+    
+    except Exception as e:
+        logger.error(f" [x] Could not get the progress for {len(task_ids)} tasks because: {e}")
+        return None
+                
