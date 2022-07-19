08fd5501de2f8fcece38a6ac9f93ece9ad443cfb
Koorosh Rezaee Araghpour <bboykoorosh@gmail.com>
2022-07-19 18:05:22 +0430

    fix: changing the default ports

--- start-coins.sh
+++ start-coins.sh
@@ -7,14 +7,14 @@ sudo docker build -t devico-coins .
 sudo docker compose up -d
 
 if [ $MYIP ]; then
-    echo "checkout the api docs at: http://$MYIP/docs"
+    echo "checkout the api docs at: http://$MYIP:8000/docs"
 else
-    echo "checkout the api docs at: http://127.0.0.1:80/docs"
+    echo "checkout the api docs at: http://127.0.0.1:8000/docs"
 fi
 if [ $MYIP ]; then
-    echo "checkout the graphql at: http://$MYIP:80/graphql"
+    echo "checkout the graphql at: http://$MYIP:8000/graphql"
 else
-    echo "checkout the graphql at: http://127.0.0.1:80/graphql"
+    echo "checkout the graphql at: http://127.0.0.1:8000/graphql"
 fi
 if [ $MYIP ]; then
     echo "checkout the rabbitmq status at: http://$MYIP:15672 with the user: guest and the password: guest"
